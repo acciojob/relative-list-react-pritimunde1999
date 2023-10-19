121a098607d7c402c03d2652e0bcaa9f400a209c
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-10-19 06:04:03 +0000

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+  const arr=["ankita","shraddha","ratish"];
+  let itemNo=1;
   return (
     <div id="main">
                {/* Do not remove the main div */}
+        
+        <ol key={"relativeList"}>
+          {
+             arr.map((ele)=>(
+              <li key={`relativeListItem${itemNo++}`}>{ele}</li>
+             ))
+          }
+        </ol>
     </div>
   )
 }
